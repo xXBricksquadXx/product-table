4e61a675caf81abbb4119ff4ceb871ee2643fecb
Colby <colby88cardell@gmail.com>
2021-02-10 17:52:18 -0500

    :sparkles: Use the store to set data

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+//
+
 import { Filters, Table } from "./components";
+import { catalog } from "./lib";
+import store from "./store";
 
 const root = document.getElementById("root");
 
 function render() {
   root.innerHTML = `
-    ${Filters()}
-    ${Table()}
+  ${Filters()}
+  ${Table()}
   `;
 }
+
+store.setData(catalog);
 
 render();
